2ab8f37566a159fdc2dd798d4fa267bd1b7a08fd
beardfriend <beardfriend21@gmail.com>
2023-03-21 10:16:09 +0900

    feat: score game restart food

--- snake/cmd/main.go
+++ snake/cmd/main.go
@@ -14,7 +14,6 @@ func main() {
 	if err != nil {
 		log.Fatalf("%+v", err)
 	}
-
 	if err := screen.Init(); err != nil {
 		log.Fatalf("%+v", err)
 	}
@@ -22,29 +21,8 @@ func main() {
 	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
 	screen.SetStyle(defStyle)
 
-	snakeParts := []snake.SnakePart{
-		{
-			X: 5,
-			Y: 10,
-		},
-		{
-			X: 6,
-			Y: 10,
-		},
-		{
-			X: 7,
-			Y: 10,
-		},
-	}
-	snakeBody := snake.SnakeBody{
-		Parts:  snakeParts,
-		Xspeed: 1,
-		Yspeed: 0,
-	}
-
 	game := snake.Game{
-		Screen:    screen,
-		SnakeBody: snakeBody,
+		Screen: screen,
 	}
 	go game.Run()
 	for {
@@ -55,14 +33,19 @@ func main() {
 			if event.Key() == tcell.KeyEscape || event.Key() == tcell.KeyCtrlC {
 				game.Screen.Fini()
 				os.Exit(0)
-			} else if event.Key() == tcell.KeyUp {
+			} else if event.Key() == tcell.KeyUp && game.SnakeBody.Yspeed == 0 {
 				game.SnakeBody.ChangeDir(-1, 0)
-			} else if event.Key() == tcell.KeyDown {
+			} else if event.Key() == tcell.KeyDown && game.SnakeBody.Yspeed == 0 {
 				game.SnakeBody.ChangeDir(1, 0)
-			} else if event.Key() == tcell.KeyLeft {
+			} else if event.Key() == tcell.KeyLeft && game.SnakeBody.Xspeed == 0 {
 				game.SnakeBody.ChangeDir(0, -1)
-			} else if event.Key() == tcell.KeyRight {
+			} else if event.Key() == tcell.KeyRight && game.SnakeBody.Xspeed == 0 {
 				game.SnakeBody.ChangeDir(0, 1)
+			} else if event.Rune() == 'y' && game.GameOver {
+				go game.Run()
+			} else if event.Rune() == 'n' && game.GameOver {
+				game.Screen.Fini()
+				os.Exit(0)
 			}
 		}
 	}
